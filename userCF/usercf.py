6ab536e4f2aaf7c8b1921bc54a019190d271e8c8
lichuan <lichuan0217@gmail.com>
2014-03-16 21:42:59 +0800

    userCF 1

---
+++ userCF/usercf.py
@@ -0,0 +1,40 @@
+import math
+
+# 余弦相似度计算
+def UserSimilarity(train):
+    # build inverse table for item_users
+    item_users = dict()
+    for u, items in train.items():
+        for i in items.keys():
+            if i not in item_users:
+                item_users[i] = set()
+            item_users[i].add(u)
+
+    # calculate co-rated items between users
+    C = dict()
+    N = dict()
+    for i, users in item_users.items():
+        for u in users:
+            N[u] += 1
+            for v in users:
+                if u == v:
+                    continue
+                c[u][v] += 1
+
+    # calculate final similarity matrix W
+    W = dict()
+    for u, related_users in C.items():
+        for v, cuv in related_users.items():
+            W[u][v] = cuv / math.sqrt(N[u] * N[v])
+
+    return W
+
+def Recommend(user, train, W):
+	rank = dict()
+	interacted_items = train[user]
+	for v, wuv in sorted(W[user].items(), key = lambda item : item[1], reverse=True)[0:K]:
+		for i, rvi in train[v].items():
+			if i in interacted_items:
+				#filter the items that the user has interacted before
+				continue
+			rank[i] += wuv * rvi
